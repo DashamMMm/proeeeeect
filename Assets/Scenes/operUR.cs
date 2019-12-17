e58c902bdc771662e6a1c5abf5cd0300a28d9bd3
RTU MIREA <rtumirea@mirea.ru>
2019-12-17 15:57:33 +0300

    кнопки для ссылок и видео

---
+++ Assets/Scenes/operUR.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class operUR : MonoBehaviour
+{
+    public string ur1;
+    public void OnClick()
+
+    {
+        Application.OpenURL(ur1);
+    }
+
+
+}
